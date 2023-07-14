387c130f8005ec7db2697bbb66b98297dbe2f015
MishelMorales02 <138738819+MishelMorales02@users.noreply.github.com>
2023-07-14 11:42:56 -0600

    subiendo

---
+++ src/proyectopoo/ProyectoPOO.java
@@ -0,0 +1,32 @@
+
+package proyectopoo;
+
+import java.util.Scanner;
+
+
+public class ProyectoPOO {
+
+  
+    public static void main(String[] args) {
+        int op = 0;
+        Scanner entrada = new Scanner(System.in);
+
+        while (op != 4) {
+            System.out.println("-----------------------------------------");
+            System.out.println("MENU PRINCIPAL");
+            System.out.println("1. Guardar contacto ");
+            System.out.println("2. Ver todos los contactos");
+            System.out.println("3. Buscar un contacto");
+            System.out.println("4. Salir");
+            System.out.println("-----------------------------------------");
+            System.out.println("       INGRESE EL NUMERO DE OPCION      ");
+            op= entrada.nextInt();
+            
+            
+           
+        
+        }
+        
+    }
+    
+}
---
+++ src/proyectopoo/contacto.java
@@ -0,0 +1,15 @@
+
+package proyectopoo;
+
+
+public class contacto {
+    String nombre, telefono, domicilio;
+
+    public contacto(String nombre, String telefono, String domicilio) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.domicilio= domicilio;
+    }
+  
+    
+}
